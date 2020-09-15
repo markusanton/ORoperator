98e7e8641251c1e109e3d701c55771e3f09015b2
markusanton <markusa070@gmail.com>
2020-09-15 13:20:05 +0300

    Add project files.

---
+++ DayCheckerTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DayCheckerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a day:");
+            string userInput = Console.ReadLine().ToLower();
+
+            if (userInput == "monday" || userInput == "tuesday" || userInput == "wednesday" || userInput == "thursday" || userInput == "friday")
+            {
+                Console.WriteLine($"{userInput} is a weekday.");
+            }
+            else if (userInput == "saturday" || userInput == "sunday")
+            {
+                Console.WriteLine($"{userInput} is the weekend.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a correct day.");
+            }
+        }
+    }
+}
---
+++ ORoperator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ORoperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What month were you born?");
+            string userMonth = Console.ReadLine().ToLower();
+
+            if (userMonth == "december" || userMonth == "january" || userMonth == "february")
+            {
+                Console.WriteLine("You were born in winter.");
+            }
+            else if (userMonth == "march" || userMonth == "april" || userMonth == "may")
+            {
+                Console.WriteLine("You were born in spring.");
+            }
+            else if (userMonth == "june" || userMonth == "july" || userMonth == "august")
+            {
+                Console.WriteLine("You were born in Summer.");
+            }
+            else if (userMonth == "september" || userMonth == "october" || userMonth == "november")
+            {
+                Console.WriteLine("You were born in fall.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a correct month.");
+            }
+
+        }
+    }
+}
