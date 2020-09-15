06326abbb91e3feedbebaf66f5a25bde1c440883
markusanton <markusa070@gmail.com>
2020-09-15 13:38:49 +0300

    switch

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -6,7 +6,33 @@ namespace Switch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter day: ");
+            string userInput = Console.ReadLine().ToLower();
+
+            switch(userInput)
+            {
+                case "sunday":
+                    Console.WriteLine("1");
+                    break;
+                case "monday":
+                    Console.WriteLine("2");
+                    break;
+                case "tuesday":
+                    Console.WriteLine("3");
+                    break;
+                case "wednesday":
+                    Console.WriteLine("4");
+                    break;
+                case "thursday":
+                    Console.WriteLine("5");
+                    break;
+                case "friday":
+                    Console.WriteLine("6");
+                    break;
+                case "saturday":
+                    Console.WriteLine("7");
+                    break;
+            }
         }
     }
 }
